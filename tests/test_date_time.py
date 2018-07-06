5f8e5b7abac35c99e7867707d74c07f82493f0a6
Zhang He <zhang_he06@163.com>
2018-07-06 20:29:40 +0800

    更新时间转换测试用例

--- tests/test_date_time.py
+++ tests/test_date_time.py
@@ -23,9 +23,16 @@ class DateTimeTest(unittest.TestCase):
     日期时间测试
     """
     def setUp(self):
+        """
+        获取系统时区, 设定一对本地时间和国际时间
+        1、断言转换后的时差是否正确
+        2、断言转换后的时间是否正确
+        :return:
+        """
         self.time_offset = time.timezone
         self.local_time = '2018-06-06 18:12:26'
-        self.utc_time = '2018-06-06 10:12:26'
+        local_time_obj = datetime.datetime.strptime(self.local_time, '%Y-%m-%d %H:%M:%S')
+        self.utc_time = (local_time_obj + datetime.timedelta(hours=self.time_offset/60/60)).strftime('%Y-%m-%d %H:%M:%S')
 
     def test_local_to_utc(self):
         """
@@ -34,7 +41,9 @@ class DateTimeTest(unittest.TestCase):
         """
         local_time_obj = datetime.datetime.strptime(self.local_time, '%Y-%m-%d %H:%M:%S')
         utc_time_obj = time_local_to_utc(self.local_time)
+
         self.assertEqual(utc_time_obj, local_time_obj + datetime.timedelta(seconds=self.time_offset))
+        self.assertEqual(self.utc_time, utc_time_obj.strftime('%Y-%m-%d %H:%M:%S'))
 
     def test_utc_to_local(self):
         """
@@ -43,7 +52,9 @@ class DateTimeTest(unittest.TestCase):
         """
         utc_time_obj = datetime.datetime.strptime(self.utc_time, '%Y-%m-%d %H:%M:%S')
         local_time_obj = time_utc_to_local(self.utc_time)
+
         self.assertEqual(utc_time_obj, local_time_obj + datetime.timedelta(seconds=self.time_offset))
+        self.assertEqual(self.local_time, local_time_obj.strftime('%Y-%m-%d %H:%M:%S'))
 
     def tearDown(self):
         pass
